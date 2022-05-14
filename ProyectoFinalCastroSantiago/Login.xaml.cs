01f429dfc36a62acef16e7bb2faa9f76e4e33bad
Hugo Santiago Castro <104536146+hugosantiagocastro90@users.noreply.github.com>
2022-05-13 22:01:09 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinalCastroSantiago/Login.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalCastroSantiago
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void btnOpen_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "";
+            string validar = validarInicio(txtusuario.Text, txtContrasena.Text);
+            if (validar == "Error")
+            {
+                DisplayAlert("Usuario o Contraseña", validar, "Continuar");
+            }
+            else
+            {
+                usuario = txtusuario.Text;
+                Navigation.PushAsync(new Registro());
+            }
+
+        }
+        private string validarInicio(string usuario, string pass)
+        {
+            string validacion = "";
+            if (usuario == "santiago2022" && pass == "uisrael2022")
+            {
+                validacion = "";
+
+            }
+            else
+            {
+                validacion = "Error";
+            }
+            return validacion;
+        }
+
+        private void btnRegistro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProyectoFinalCastroSantiago/Paquetes.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalCastroSantiago
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Paquetes : ContentPage
+    {
+        public Paquetes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
